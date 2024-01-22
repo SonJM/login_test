a47fa6bf240e38fead49895275f7586d75154024
SonJM <blisle@naver.com>
2024-01-23 00:52:10 +0900

    이ã건 왜 연결이 안되는가

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+body{
+  padding: 10px;
+  margin: 10px;
+}
+</style>
